48f33a4e42153fe8a4f084b0d338682ed3323586
inesh-l <56318873+inesh-l@users.noreply.github.com>
2022-12-22 10:56:56 -0800

    created dev branch

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from models import Profile
+from . import models
+from django.contrib.auth.models import User
 
-# Register your models here.
-admin.site.register(Profile)
+
+class ProfileInline(admin.StackedInline):
+    model = models.Profile
+
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inlines = [ProfileInline]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
